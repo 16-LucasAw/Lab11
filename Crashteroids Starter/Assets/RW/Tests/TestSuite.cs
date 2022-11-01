bb768343374b88f3835ca557f5551a66ffcb606f
16-LucasAw <RealAkaliMainer@gmail.com>
2022-11-01 10:52:57 +0800

    Added Setup and Teardown

--- Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
+++ Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
@@ -11,6 +11,20 @@ namespace Tests
     {
         private Game game;
 
+        [SetUp]
+        public void Setup()
+        {
+            GameObject gameGameObject =
+                MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+            game = gameGameObject.GetComponent<Game>();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Object.Destroy(game.gameObject);
+        }
+
         // 1
         [UnityTest]
         public IEnumerator AsteroidsMoveDown()
